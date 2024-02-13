bf1f71b4d3cea100442ae4101774e2c6516dd618
Camila Vives <camvives@gmail.com>
2024-02-13 12:27:02 -0300

    Master view

--- puntos/views.py
+++ puntos/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.template import loader
+from django.http import HttpResponse
+from .models import Entrada
 
 # Create your views here.
+def index(request):
+    entry_list = Entrada.objects.order_by("fecha")
+    template = loader.get_template('puntos/index.html')
+    context = {
+        "entry_list": entry_list,
+    }
+    return HttpResponse(template.render(context, request))
